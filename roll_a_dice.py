c3dfbf8b59e0b99eb8471d49e5418fe8c05d243a
SarveshHardas <86554291+SarveshHardas@users.noreply.github.com>
2022-07-29 07:47:30 +0530

    Add files via upload

---
+++ roll_a_dice.py
@@ -0,0 +1,56 @@
+import random
+response=input('Press y to start or n to end(y/n): ')
+
+while response=='y':
+    no=random.randint(1,6)
+    if(no==1):
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    response
+
+    elif:
+        print("[-----]")
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+        print("[-----]")
+    response
+
+    elif:
+        print("[-----]")
+        print("[0    ]")
+        print("[  0  ]")
+        print("[    0]")
+        print("[-----]")
+    response
+
+    elif:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    response
+
+    elif:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    response
+
+    else:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+    response
+
+while response=='n':
+    print("Thanks For playing")
+
